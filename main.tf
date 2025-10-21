a719edc2edd1e6431b64675bf3faae83e5484d8d
yosef781227 <joc781227@gmail.com>
2025-10-21 13:59:09 +0000

    tf var moved to root and remove the tf var in the module

--- main.tf
+++ main.tf
@@ -8,6 +8,58 @@ terraform {
   required_version = ">=1.3.0"
 }
 
+# Variables
+variable "app_service_plan_name" {
+  type        = string
+  description = "App Service Plan name"
+}
+
+variable "webapp_name" {
+  type        = string
+  description = "Unique Web App name"
+}
+
+variable "docker_image" {
+  type        = string
+  description = "Docker image name"
+}
+
+variable "docker_image_tag" {
+  type        = string
+  description = "Docker image tag"
+  default     = "latest"
+}
+
+variable "sku_name" {
+  type        = string
+  description = "App Service Plan SKU"
+  default     = "B1"
+}
+
+variable "https_only" {
+  type        = bool
+  description = "Force HTTPS only"
+  default     = true
+}
+
+variable "always_on" {
+  type        = bool
+  description = "Keep the app always on"
+  default     = true
+}
+
+variable "container_port" {
+  type        = number
+  description = "Container port"
+  default     = 80
+}
+
+variable "app_settings" {
+  type        = map(string)
+  description = "Application settings"
+  default     = {}
+}
+
 provider "azurerm" {
   features {}
   # Service Principal authentication via environment variables:
@@ -25,4 +77,13 @@ module "webapp" {
   source                = "./modules/webapp-container"
   resource_group_name   = azurerm_resource_group.rg.name
   location              = azurerm_resource_group.rg.location
+  app_service_plan_name = var.app_service_plan_name
+  webapp_name           = var.webapp_name
+  docker_image          = var.docker_image
+  docker_image_tag      = var.docker_image_tag
+  sku_name              = var.sku_name
+  https_only            = var.https_only
+  always_on             = var.always_on
+  container_port        = var.container_port
+  app_settings          = var.app_settings
 }
